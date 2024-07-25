caa05007eb4b3f7c71d2c11425646ce369ab1ed5
grigadnal625 <grigandal625@gmail.com>
2024-07-25 14:21:17 +0300

    add try-catch to pidfile

--- at_queue/__main__.py
+++ at_queue/__main__.py
@@ -22,12 +22,14 @@ async def main(**connection_kwargs):
     registry = ATRegistry(connection_parameters)
 
     await registry.initialize()
+    try:
+        if not os.path.exists('/var/run/at_queue/'):
+            os.makedirs('/var/run/at_queue/')
 
-    if not os.path.exists('/var/run/at_queue/'):
-        os.makedirs('/var/run/at_queue/')
-
-    with open('/var/run/at_queue/pidfile.pid', 'w') as f:
-        f.write(str(os.getpid()))
+        with open('/var/run/at_queue/pidfile.pid', 'w') as f:
+            f.write(str(os.getpid()))
+    except PermissionError:
+        pass
         
     await registry.start()
 
